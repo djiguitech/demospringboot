517c88e460c1d55d745e89e67750a0e1f844b17e
SANOGOTECH <sanogotech@yahoo.fr>
2017-12-08 12:19:32 +0000

    Add files via upload

--- src/main/java/com/cnss/demo/SpringBootDemoApplication.java
+++ src/main/java/com/cnss/demo/SpringBootDemoApplication.java
@@ -1,20 +1,14 @@
 package com.cnss.demo;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-import com.cnss.demo.service.GenreServiceHelper;
 
-@ComponentScan
 @SpringBootApplication
 public class SpringBootDemoApplication implements CommandLineRunner {
 
-	@Autowired
-	GenreServiceHelper genreServiceHelper;
 	
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
@@ -22,7 +16,7 @@ public class SpringBootDemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		//genreServiceHelper.saveDefaultGenre();
+	
 	}
    
    
